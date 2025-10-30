a8092d2b793eec98b401d16048be3d8acb237197
Ebube Onaga <31213368+onaga04@users.noreply.github.com>
2025-10-30 10:23:52 -0500

    Update run_backup.sh

--- run_backup.sh
+++ run_backup.sh
@@ -32,7 +32,7 @@ while true; do
       if (( choice >= 1 && choice <= ${#tasks[@]} )); then
         task_name="${tasks[$((choice-1))]}"
         echo "Running: $task_name"
-        ansible-playbook -i inventory.ini ansible/velero-backup.yaml --tags "$(echo "$task_name" -vvvv | tr ' ' '_')" || {
+        ansible-playbook -i inventory.ini ansible/velero-backup.yaml --tags "$(echo "$task_name" | tr ' ' '_')" -vvvv || {
           echo "Error running $task_name"
         }
       else
